f88cb23f502381dfba24fcdbce03ae4cabaecd5d
Md. Nihad Hossain <77912479+GoesToTwentyOne@users.noreply.github.com>
2021-12-01 10:34:32 +0600

    Add files via upload
    lab -2

---
+++ keyboard intput.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    //summation
+    printf("Sum two numbers--------------\n");
+    int a,b,sum;
+    printf("Enter Number1:\n");
+    scanf("%d", &a);
+    printf("Enter Number2:\n");
+    scanf("%d", &b);
+    sum=a+b;
+    printf("%d + %d = %d",a,b,sum);
+    return 0;
+}
