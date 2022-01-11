2827a3455c200bfcebccbcf2d9a8b597a8fa2473
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-01-11 21:01:19 +0600

    char array inpur with scanf

--- Lab-6/01_char_array_input/char.c
+++ Lab-6/01_char_array_input/char.c
@@ -1,1 +1,10 @@
 
+#include <stdio.h>
+int main()
+{
+    char without_space_name[50];
+    printf("Enter  your name: ");
+    scanf("%s", without_space_name);
+    printf("Your name is %s.", without_space_name);
+    return 0;
+}
